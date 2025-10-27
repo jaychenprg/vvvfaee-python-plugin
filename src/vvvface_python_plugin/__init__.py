d46d672cae981cf0fa804696bf5eb2b71f416fcd
Jay <jaychenprg@gmail.com>
2025-10-27 12:57:57 +0800

    注册路由

--- src/vvvface_python_plugin/__init__.py
+++ src/vvvface_python_plugin/__init__.py
@@ -1,2 +1,14 @@
+from aiohttp import web
+
+from .routes import create_openai_request_app
+
+app = web.Application()
+
+def create_app() -> web.Application:
+    app = web.Application()
+    app.add_subapp('/pyapi/vvv-face', create_openai_request_app())
+    return app
+
 def main() -> None:
     print("Hello from vvvface-python-plugin!")
+    web.run_app(create_app(), host="localhost", port=8001)
